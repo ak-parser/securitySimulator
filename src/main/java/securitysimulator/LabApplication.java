b8a8d71cb64dd692d21ad7c591cc7543600d81b0
Danylo Cherniavskyi <danylo.chernyavskiy@gmail.com>
2022-11-17 08:58:02 +0200

    Add thread initialization

--- src/main/java/securitysimulator/LabApplication.java
+++ src/main/java/securitysimulator/LabApplication.java
@@ -4,10 +4,40 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import securitysimulator.Models.Building;
+import securitysimulator.Thread.*;
 
 import java.io.IOException;
 
 public class LabApplication extends Application {
+    private ViolationGeneratorThread violationGeneratorThread;
+    private ViolationHandlerThread violationHandlerThread;
+    private Building building;
+
+    private void createHandlerThread() {
+        violationHandlerThread = new ViolationHandlerThread(building);
+        violationHandlerThread.run();
+    }
+
+    private void createGeneratorThread() {
+        violationGeneratorThread = new ViolationGeneratorThread(building);
+        violationGeneratorThread.run();
+    }
+
+    private void killGeneratorThread() {
+        violationGeneratorThread.kill();
+    }
+
+    public void onClickPlay() {
+        createGeneratorThread();
+    }
+
+    public void onClickPause() {
+        killGeneratorThread();
+    }
+
+    public void onClick() {}
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LabApplication.class.getResource("hello-view.fxml"));
@@ -15,6 +45,8 @@ public class LabApplication extends Application {
         stage.setTitle("Security simulator!");
         stage.setScene(scene);
         stage.show();
+
+        createHandlerThread();
     }
     public static void main(String[] args) {
         launch();
