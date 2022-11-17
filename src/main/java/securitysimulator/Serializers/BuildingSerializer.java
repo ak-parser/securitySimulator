2060df746917dfca04b70a2431dde51251ff1083
sashasush22 <sashasush228@gmail.com>
2022-11-17 08:55:53 +0200

    Added serializer

---
+++ src/main/java/securitysimulator/Serializers/BuildingSerializer.java
@@ -0,0 +1,15 @@
+package securitysimulator.Serializers;
+
+import securitysimulator.Models.Building;
+
+import java.io.File;
+
+public class BuildingSerializer {
+    public static void Serialize(File file, Building building){
+
+    }
+
+    public static void Deserialize(File file, Building building){
+
+    }
+}
